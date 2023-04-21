8cd6bde91a0be163a7f0c6eecab657e114ed9a43
irinadica <irina_andreea_dica@yahoo.com>
2023-04-21 18:34:28 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/h&f.css">
+</head>
+<body>
+    <?php
+        include ("top.php");
+    ?>
+    <main>
+        <div class="boxes">
+                        <div class="box"><span>Fun stuff</span></div>
+                        <div class="box"><span>Serious stuf</span>f</div>
+                        <div class="box"><span>Boring Stuff</span></div>
+                        <div class="box"><span>Interesting Stuff</span></div>
+        </div>
+    </main>
+    <?php
+        include ("bottom.php");
+    ?>
+</body>
+</html>
