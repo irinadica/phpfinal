6f0baa73de50bc22c7d14d38c310ee717e00ac89
irinadica <irina_andreea_dica@yahoo.com>
2023-04-21 18:36:36 +0100

    Create bottom.php

---
+++ bottom.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="css/h&f.css">
+</head>
+<body>
+    <footer>
+            <div class="contact_details">
+                <span>You can also find us at:<span>
+                <span>SAE House, 297 Kingsland Rd, London E8 4DD</span>
+                <span>0333 011 2315</span>
+            </div>
+            <div class="sm_links">
+                <div id="lin">
+                    <a href="https://uk.linkedin.com/company/sae-institute-london">
+                        <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="linkedin logo">
+                    </a>
+                    <span>LinkedIn</span>
+                </div>
+                <div id="official_website">
+                    <a href="https://www.sae.edu/gbr/contact-us">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/0/07/SAE_Institute_Logo.png" alt="sae logo">
+                    </a>
+                    <span>Official Website</span>
+                </div>
+                <div id="instagram">
+                    <a href="https://www.instagram.com/sae_institute_uk/">
+                        <img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-icon-png-image_6315974.png" alt="instagram logo">
+                    </a>
+                    <span>Instagram</span>
+                </div>
+            </div>
+    </footer>
+</body>
+</html>
