d46586934ef1102c4838867cf6ab5aeb63123341
irinadica <irina_andreea_dica@yahoo.com>
2023-04-21 18:36:39 +0100

    Create connapi.php

---
+++ crud/connapi.php
@@ -0,0 +1,17 @@
+<?php
+
+    $serverName="localhost";
+    $username="root";
+    $password="";
+    $database="sae";
+
+    $conn=mysqli_connect($serverName,$username,$password,$database);
+
+
+    
+
+
+
+
+
+?>
